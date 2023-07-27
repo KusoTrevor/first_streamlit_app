81d8fd52d1e9485eb2e3e376fbd93ae2c1741d34
KusoTrevor <140689665+KusoTrevor@users.noreply.github.com>
2023-07-27 00:32:12 -0700

    Update streamlit_app.py
    Updated header content

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My first Streamlit app")
 streamlit.text("testing the Text function")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
