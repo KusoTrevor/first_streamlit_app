fd70453de030e8c00f8467a74d36122de7fd7271
KusoTrevor <140689665+KusoTrevor@users.noreply.github.com>
2023-07-27 00:27:50 -0700

    Update streamlit_app.py
    Added breakfast menu section

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title("My first Streamlit app")
 streamlit.text("testing the Text function")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
